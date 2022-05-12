afd2edce7fe6d286bf2fe387a665be1d3b39a994
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-05-12 10:50:38 +0900

    Initial Commit

---
+++ Shared/ClipboardLandingPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardLandingPageApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/05/12.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardLandingPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
